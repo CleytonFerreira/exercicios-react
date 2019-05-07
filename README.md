be2063f57b6884381d4beb549ad038fe0967b35a
CleytonFerreira <jcleyton03@gmail.com>
2019-05-07 13:30:55 -0300

    refatoração

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Exercícios ReactJS
 
-Exercícios do curso [React + Redux](https://www.udemy.com/react-redux-pt/), disponível em [Udemy.com](https://www.udemy.com)
+Exercícios ReactJs
--- src/components/Pagina3.jsx
+++ src/components/Pagina3.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ComponenteClasse from '../components/ComponenteClasse';
 import Contador from '../components/Contador';
 import Hook from '../components/Hook';
+import Clock from '../components/Clock'
 
 export default () => (
     <React.Fragment>
          <h1>Esta é a Página 3</h1>
          <ComponenteClasse valor="Sou um componente de classe"></ComponenteClasse>
          <Contador numero={0}></Contador>
-         <Hook></Hook> 
+         <Hook></Hook>
+        <Clock />
     </React.Fragment>
    
 );
